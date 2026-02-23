586e4c90b47b434d6e35cca97008167e5b15596e
ismaelnata01-maker <ismaelnata01@gmail.com>
2026-02-23 17:15:53 -0300

    Add files via upload

---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Projects(){
+    return(
+        <main className="h-screen w-screen bg-amber-950 flex justify-center">
+            <h1>Meus Projetos</h1>
+        </main>
+    );
+}
