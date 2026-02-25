5a0616c16c8be825a96fea3e3805821e750bce79
ismaelnata01-maker <ismaelnata01@gmail.com>
2026-02-25 16:58:57 -0300

    Add files via upload

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,16 @@
 import Card from "@/components/Card";
 export default function Projects(){
     return(
-        <main className="h-screen w-screen bg-amber-950 flex justify-center">
-            <h1>Meus Projetos</h1>
-            
-            <Card />
+        <main className="h-screen w-screen bg-amber-950 flex items-center flex-col gap-4">
+            <h1 className="text-6xl mt-4">Meus Projetos</h1>
+
+
+            <Card src="/lobo4e4.webp" title="lp-nova" description="Site informativo sobre lobos."url="https://github.com/ismaelnata01-maker/lp-nova"/>
+
+            <Card src="/cama-elastica1.jpg" title="Serviços" description="Site sobre serviços de uma empresa que aluga brinquedos." url="https://github.com/ismaelnata01-maker/servico-forja-tarde-Ismael_e_Vinicius"/>
+
+            <Card src="/file.svg" title="Portfolio" description="Meu portfolio feito em React e Next.js." url="https://github.com/ismaelnata01-maker/portfolio-forja-tarde"/>
+
         </main>
     );
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,15 +1,31 @@
 import Image from "next/image"
-export default function Card(){
+import Link from "next/link"
+
+interface Props {
+    src: string;
+    title: string;
+    description: string;
+    url: string;
+}
+
+export default function Card({src, title, description, url}: Props){
     return(
-    <div className="flex flex-col h-48 w-48 rounded-lg bg-white">
+
+       <Link href={url} target="_blank" rel="noopener noreferrer">
+       
+    <div className="flex flex-col h-64 w-64 rounded-lg bg-white">
         <div className="flex-1 relative">
             <Image 
-            src="/eu.png"
+            src={src}
             alt="imagem do projeto"
             fill
             />
         </div>
-        <div className="flex-1"></div>
+        <div className="flex-1 border-t border-black mt-1">
+            <h2 className="text-black text-center text-3xl">{title}</h2>
+            <p className="text-black">{description}</p>
+        </div>
     </div>
+    </Link>
     )
 }
