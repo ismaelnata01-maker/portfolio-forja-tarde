b50e2a8c81b8dde3153e6f4e4b63abaebff93dd3
ismaelnata01-maker <ismaelnata01@gmail.com>
2026-03-02 15:37:37 -0300

    Add files via upload

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,6 +8,13 @@ export default function Contact(){
     }, [email]);
     
 
+
+    /*useState= Cria variável q será usada em todo componente ou pagina*/
+    /*useEffect= Faz ação de acordo com a mudança de um valor*/
+    /*useMemo= Muda o valor de uma variavel de acordo com o valor de outra variavel*/
+
+
+
     /*const [error, setError] = useState(false);
 
     useEffect(()=>{
@@ -17,6 +24,20 @@ export default function Contact(){
             setError(true)
         }
     },[email]);*/
+
+    function getEmail(){
+        /*console.log(email);*/
+    
+
+    if(!error){/*Isso é o mesmo q dizer error = false */
+        const msg = `Gostaria de entrar em contato!\n\nEmail: ${email}`
+    window.open(`https://wa.me/5541999999999?text=${msg}`)
+    }else{
+        alert("Digite um email válido.")
+    }
+}
+    
+    
 
     return(
         <main className="W-screen h-screen flex flex-col gap-16 items-center justify-center">
@@ -31,10 +52,10 @@ export default function Contact(){
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             />
-            {error && <p>Email inválido</p>}
+            {error && <p className="text-red-500">Email inválido</p>}
             </div>
 
-            <button className="cursor-pointer border border-white py-2 px-10 rounded-full">Enviar</button>
+            <button className="cursor-pointer border border-white py-2 px-10 rounded-full disabled:cursor-not-allowed disabled:opacity-50" onClick={() => getEmail() }disabled={error}>Enviar</button>
         </main>
     )
 }
